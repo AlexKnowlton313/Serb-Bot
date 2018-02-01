94a12dea74a821c3ae6efb389f05a35e4ef3747c
Alex Knowlton <alexrules313@gmail.com>
2018-02-01 00:21:34 -0500

    added emitter for stats

--- Serb.js
+++ Serb.js
@@ -28,11 +28,44 @@ client.on('message', message => {
 		if (args.length === 0) {
 			message.channel.send("The `!stats` command must be followed by a username");
 		} else {
+			message.channel.send(`Fetching stats for ${args[0]}, one moment`);
 			// get the account id by username
-			wot.get('account/list', { search: args[0] }).then((response) => {
+			wot.get('account/list', { search: args[0] }).then((userList) => {
+				const accountID = userList.data[0].account_id;
 				// get the account stats by username
-				wot.get('account/info', { account_id: response.data[0].account_id }).then((response) => {
-					console.log(response.data);
+				wot.get('account/info', { account_id: accountID }).then((userInfo) => {
+					const userStatistics = userInfo.data[Object.keys(userInfo.data)[0]];
+					const gameStatistics = userStatistics.statistics.all;
+					console.log(gameStatistics);
+
+					// last battle played
+					var lastBattleTime = new Date(0); // The 0 there is the key, which sets the date to the epoch
+					lastBattleTime.setUTCSeconds(userStatistics.last_battle_time);
+
+					// games played
+					const gamesPlayed = gameStatistics.battles;
+
+					// win percentage
+					const winPercentage = (gameStatistics.wins / gamesPlayed).toFixed(2);
+
+					// average damage
+					const averageDamage = (gameStatistics.damage_dealt / gamesPlayed).toFixed(2);
+
+					// average kills
+					const averageKills = (gameStatistics.frags / gamesPlayed).toFixed(2);
+
+					const wotStats = new Discord.RichEmbed()
+						.setAuthor(`WoT Statistics for ${args[0]}`, message.author.avatarURL)
+						.addField(`Total Games Played`, gamesPlayed, true)
+						.addField(`Win Percentage`, winPercentage, true)
+						.addField(`Kills Per Game`, averageKills, true)
+						.addField(`Damage Per Game`, averageDamage, true)
+						.addField(`Last battle`, formatDate(lastBattleTime), true)
+						.setColor([219, 16, 41])
+						.setFooter("Taken from WargamingAPI")
+						.setTimestamp();
+
+						message.channel.send({ embed: wotStats });
 				}).catch((error) => {
 					console.log(error.message);
 					message.reply(`I was unable to find WoT data for ${args[0]}`);
@@ -43,8 +76,6 @@ client.on('message', message => {
 				message.reply(`I was unable to find WoT data for ${args[0]}`);
 				return;
 			});
-
-			message.channel.send(args[0]);
 		}
 	}
 
@@ -81,6 +112,22 @@ client.on('guildMemeberAdd', member => {
 	// set the role
 	member.addRole(proletarianRole).catch(console.error);
 })
+
+// date formatter
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return monthNames[monthIndex] + ' ' + day + ', ' + year;
+}
 
 // Log the bot in using our token: https://discordapp.com/developers/applications/me/408384681004761108
 client.login(config.token);
